95b633ee69cf91b60900bd9c4953141a1be4eff0
OmegaDracul <salimabdu008@gmail.com>
2019-05-25 21:47:05 +0100

    Worked on the SignUp page

--- lib/login/login.dart
+++ lib/login/login.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:yafe/login/signUp.dart';
 
 class Login extends StatefulWidget {
   @override
@@ -15,7 +16,7 @@ class _LoginState extends State<Login> {
         child: Form(
           child: SingleChildScrollView(
             child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.start,
               children: <Widget>[
                 // Container with the logo
                 Padding(
@@ -34,8 +35,9 @@ class _LoginState extends State<Login> {
                   child: Column(
                     // Containing the user login
                     children: <Widget>[
+                      // Contains Username
                       Padding(
-                        padding: const EdgeInsets.all(6.0),
+                        padding: const EdgeInsets.all(4.0),
                         child: TextFormField(
                           keyboardType: TextInputType.text,
                           decoration: InputDecoration(
@@ -48,8 +50,9 @@ class _LoginState extends State<Login> {
                           ),
                         ),
                       ),
+                      // Contains password
                       Padding(
-                        padding: const EdgeInsets.all(6.0),
+                        padding: const EdgeInsets.fromLTRB(6, 4, 6, 0),
                         child: TextFormField(
                           keyboardType: TextInputType.text,
                           decoration: InputDecoration(
@@ -67,8 +70,24 @@ class _LoginState extends State<Login> {
                           obscureText: true,
                         ),
                       ),
+                      Align(
+                        alignment: Alignment.centerLeft,
+                        child: Padding(
+                          padding: const EdgeInsets.all(0.0),
+                          child: FlatButton(
+                            onPressed: () {},
+                            child: Text(
+                              "Forgot password?",
+                              textAlign: TextAlign.start,
+                              style: TextStyle(
+                                  fontSize: 12, color: Colors.red[800]),
+                            ),
+                          ),
+                        ),
+                      ),
+                      // Login button
                       Padding(
-                        padding: EdgeInsets.fromLTRB(0, 45, 0, 20),
+                        padding: EdgeInsets.fromLTRB(0, 20, 0, 16),
                         child: Container(
                           width: 100,
                           height: 45,
@@ -76,7 +95,8 @@ class _LoginState extends State<Login> {
                             elevation: 4,
                             color: Colors.white,
                             shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(5)),
+                              borderRadius: BorderRadius.circular(5),
+                            ),
                             child: Text(
                               "Login",
                               style: TextStyle(
@@ -90,6 +110,7 @@ class _LoginState extends State<Login> {
                         color: Colors.grey[600],
                         indent: 6,
                       ),
+                      // Login Alternative
                       Padding(
                         padding: const EdgeInsets.all(8.0),
                         child: FlatButton(
@@ -108,10 +129,15 @@ class _LoginState extends State<Login> {
                           onPressed: () {},
                         ),
                       ),
+                      // Sign-up button
                       Padding(
-                        padding: EdgeInsets.only(top: 16),
+                        padding: EdgeInsets.only(top: 12),
                         child: FlatButton(
-                          onPressed: () {},
+                          onPressed: () {
+                            Route route = MaterialPageRoute(
+                                builder: (context) => SignUp());
+                            Navigator.push(context, route);
+                          },
                           child: Text(
                             "Don't have an account yet? Sign-up here",
                             style: TextStyle(color: Colors.red[900]),
@@ -129,76 +155,3 @@ class _LoginState extends State<Login> {
     );
   }
 }
-
-/* MyHomePage({Key key, this.title}) : super(key: key);
-
-    // This widget is the home page of your application. It is stateful, meaning
-    // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title; */
-
-/* int _counter = 0;
-
-  void _incrementCounter() {
-	setState(() {
-	  // This call to setState tells the Flutter framework that something has
-	  // changed in this State, which causes it to rerun the build method below
-	  // so that the display can reflect the updated values. If we changed
-	  // _counter without calling setState(), then the build method would not be
-	  // called again, and so nothing would appear to happen.
-	  _counter++;
-	});
-  } */
-
-// This method is rerun every time setState is called, for instance as done
-// by the _incrementCounter method above.
-//
-// The Flutter framework has been optimized to make rerunning build methods
-// fast, so that you can just rebuild anything that needs updating rather
-// than having to individually change instances of widgets.
-
-/* appBar: AppBar(
-		// Here we take the value from the MyHomePage object that was created by
-		// the App.build method, and use it to set our appbar title.
-		title: Text(widget.title),
-	  ), */
-
-// Center is a layout widget. It takes a single child and positions it
-// in the middle of the parent.
-
-// Column is also layout widget. It takes a list of children and
-// arranges them vertically. By default, it sizes itself to fit its
-// children horizontally, and tries to be as tall as its parent.
-//
-// Invoke "debug painting" (press "p" in the console, choose the
-// "Toggle Debug Paint" action from the Flutter Inspector in Android
-// Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-// to see the wireframe for each widget.
-//
-// Column has various properties to control how it sizes itself and
-// how it positions its children. Here we use mainAxisAlignment to
-// center the children vertically; the main axis here is the vertical
-// axis because Columns are vertical (the cross axis would be
-// horizontal).
-
-/* children: <Widget>[
-			Text(
-			  'You have pushed the button this many times:',
-			),
-			Text(
-			  '$_counter',
-			  style: Theme.of(context).textTheme.display1,
-			),
-		  ]*/
-
-/* floatingActionButton: FloatingActionButton(
-		onPressed: _incrementCounter,
-		tooltip: 'Increment',
-		child: Icon(Icons.add),
-	  ), // This trailing comma makes auto-formatting nicer for build methods. */
--- lib/login/signUp.dart
+++ lib/login/signUp.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class SignUp extends StatefulWidget {
@@ -8,6 +9,137 @@ class SignUp extends StatefulWidget {
 class _SignUpState extends State<SignUp> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      resizeToAvoidBottomPadding: true,
+      body: Container(
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(0, 14, 0, 2),
+                    child: BackButton(),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(0, 14, 12, 2),
+                    child: Align(
+                      alignment: Alignment.topRight,
+                      child: Icon(Icons.donut_small),
+                    ),
+                  )
+                ],
+              ),
+              Container(
+                child: Column(
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(0, 10, 0, 25),
+                      child: Center(
+                        child: Material(
+                            shape: CircleBorder(),
+                            color: Colors.white10,
+                            child: IconButton(
+                              color: Colors.blueGrey[100],
+                              icon: Icon(Icons.account_circle),
+                              iconSize: 150,
+                              onPressed: () {},
+                            ) /* FlatButton(
+                            onPressed: () {},
+                            child: Icon(
+                              Icons.account_circle,
+                              color: Colors.blueGrey[100],
+                              size: 150,
+                            ), */
+                            ),
+                      ),
+                    ),
+                    Form(
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 32),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: TextFormField(
+                                keyboardType: TextInputType.text,
+                                decoration: InputDecoration(
+                                  focusedBorder: OutlineInputBorder(),
+                                  labelStyle: TextStyle(color: Colors.black),
+                                  labelText: "Username",
+                                  border: OutlineInputBorder(),
+                                ),
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: TextFormField(
+                                keyboardType: TextInputType.text,
+                                decoration: InputDecoration(
+                                  focusedBorder: OutlineInputBorder(),
+                                  labelStyle: TextStyle(color: Colors.black),
+                                  labelText: "Password",
+                                  border: OutlineInputBorder(),
+                                ),
+                                obscureText: true,
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: TextFormField(
+                                keyboardType: TextInputType.text,
+                                decoration: InputDecoration(
+                                  focusedBorder: OutlineInputBorder(),
+                                  labelStyle: TextStyle(color: Colors.black),
+                                  labelText: "Confirm Password",
+                                  border: OutlineInputBorder(),
+                                ),
+                                obscureText: true,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 24),
+                      child: Container(
+                        width: 120,
+                        height: 45,
+                        child: RaisedButton(
+                          elevation: 4,
+                          color: Colors.white,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(5),
+                          ),
+                          child: Text(
+                            "SIGN-UP",
+                            style:
+                                TextStyle(color: Colors.red[800], fontSize: 18),
+                          ),
+                          onPressed: () {},
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(20, 30, 10, 0),
+                      child: Center(
+                        child: Text(
+                          "By signing up, you are agreeing to our terms and conditions & privay policy.",
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
